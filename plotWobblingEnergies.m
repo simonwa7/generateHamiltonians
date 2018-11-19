059f36fa64909ee411be910ef946bb085b231242
simonwa7 <simonwa7@gmail.com>
2018-11-19 09:46:43 -0800

    adding data, plots, and figures

--- plotWobblingEnergies.m
+++ plotWobblingEnergies.m
@@ -1,33 +1,52 @@
-function plotWobblingEnergies(EnergyData, MAX_ANGLE, MIN_LENGTH, MAX_LENGTH, ITERATIONS)
+function plotWobblingEnergies(EnergyData, MIN_ANGLE, MAX_ANGLE, MIN_LENGTH, MAX_LENGTH, ITERATIONS)
 %EnergyData should be a matrix where each row is of
 % the form "middleLength, angle, eigenvalue, hf_energy"
 %And the procession of the data goes:
 %   for each length:
 %       for each angle:
 %           middleLength angle eigenvalue hf_energy
-
-angles = zeros(MAX_ANGLE,1);
-lengths = zeros(ITERATIONS,1);
+[max_iter, datacols] = size(EnergyData);
+max_iter = max_iter/(MAX_ANGLE-MIN_ANGLE);
+angles = zeros(MAX_ANGLE-MIN_ANGLE,1);
+lengths = zeros(max_iter,1);
 
 datapoint = 1;
-EigenValues = zeros(MAX_ANGLE, ITERATIONS);
-HartreeFockEnergies = zeros(MAX_ANGLE, ITERATIONS);
-for i = 1:MAX_ANGLE
-    angles(i) = (i-1)*10;
-    for j = 1:ITERATIONS
-        lengths(j) = MIN_LENGTH+(j-1)*(MAX_LENGTH-MIN_LENGTH)/ITERATIONS;
+EigenValues = zeros(max_iter, MAX_ANGLE-MIN_ANGLE);
+HartreeFockEnergies = zeros(max_iter, MAX_ANGLE-MIN_ANGLE);
+
+for i=1:max_iter
+    lengths(i) = MIN_LENGTH+(i-1)*(MAX_LENGTH-MIN_LENGTH)/ITERATIONS;
+    for j=1:MAX_ANGLE-MIN_ANGLE
+        angles(j) = (j-1+MIN_ANGLE)*10;
         EigenValues(i, j) = EnergyData(datapoint, 3);
         HartreeFockEnergies(i, j) = EnergyData(datapoint, 4); 
         datapoint = datapoint + 1;
     end
 end
+        
+
+
+%for i = 1:MAX_ANGLE
+ %   angles(i) = (i-1)*10;
+  %  for j = 1:ITERATIONS
+   %     lengths(j) = MIN_LENGTH+(j-1)*(MAX_LENGTH-MIN_LENGTH)/ITERATIONS;
+    %    EigenValues(i, j) = EnergyData(datapoint, 3);
+     %   HartreeFockEnergies(i, j) = EnergyData(datapoint, 4); 
+      %  datapoint = datapoint + 1;
+    %end
+%end
+
+EigenValues = EigenValues';
+HartreeFockEnergies = HartreeFockEnergies';
+angles = angles';
+lengths = lengths';
 
 % Surface plot
 figure;
 surf(lengths, angles, EigenValues)
 hold on;
 surf(lengths, angles, HartreeFockEnergies)
-xlabel('length'); ylabel('angle');
+xlabel('length'); ylabel('angle');zlabel('energy');
 
 % Contour plot
 %figure;
